0ef80337a2d4d993cdcba5bf423468e61ac6bc10
Bahy Adel <bahyadel515@gmail.com>
2025-05-15 23:43:16 +0300

    ...

--- Back-end/DB_config/database.ts
+++ Back-end/DB_config/database.ts
@@ -1,16 +1,39 @@
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config(); 
+// const database = ()=>{
+     
+//     mongoose.connect(process.env.MONGODB_URI!).then(
+//         ()=>{
+//             console.log(`database connected to ${process.env.MONGODB_URI}`);
+//         }).catch(  (err:Error)=>{
+//             console.log(err);
+//         });
+// }
+
+
+// export default database ;
+
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config(); 
-const database = ()=>{
-     
-    mongoose.connect(process.env.DB!).then(
-        ()=>{
-            console.log(`database connected to ${process.env.MONGODB_URI}`);
-        }).catch(  (err:Error)=>{
-            console.log(err);
-        });
-}
+dotenv.config();
 
+const database = () => {
+  const uri = process.env.MONGODB_URI || process.env.DB;
 
-export default database ;
+  if (!uri) {
+    throw new Error(" MongoDB URI is missing in environment variables!");
+  }
+
+  mongoose.connect(uri)
+    .then(() => {
+      console.log(`Database connected to: ${uri}`);
+    })
+    .catch((err: Error) => {
+      console.error("Database connection error:", err);
+    });
+};
+
+export default database;
