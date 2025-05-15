f09cc1c2c1b9c8f2bae06b1add2fb9a8bcb23825
Bahy Adel <bahyadel515@gmail.com>
2025-05-16 00:10:51 +0300

    deploy angular

--- frontend/src/app/services/global.service.ts
+++ frontend/src/app/services/global.service.ts
@@ -6,12 +6,12 @@ import { Injectable } from '@angular/core';
 export class GlobalService {
 
 
-  hostName: string = 'http://localhost:3000';
+  // hostName: string = 'http://localhost:3000';
+  hostName: string = 'https://atc-01224858865-last.vercel.app';
   authRoute: string = '/api/auth';
   eventRoute:string= '/api/events';
   eventImages: string = `${this.hostName}`
   bookRoute:string= '/api/booking';
-  // certificatesRoute:string= '/api/certificates';
 
   constructor() { }
 
